523b69f47f8090f2bf19fbed707510c35fdf96c0
AndreiMariusZed <62403550+AndreiMariusZed@users.noreply.github.com>
2021-12-10 02:11:35 +0200

    initial commit

---
+++ server/server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const morgan = require("morgan");
+
+const app = express();
+
+//Middlewares
+app.use(morgan("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.get("/", (req, res) => {
+  res.json("merge");
+});
+
+app.post("/", (req, res) => {
+  console.log(req.body.name);
+});
+
+app.listen(3000, (err) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log("Listening on PORT", 3000);
+  }
+});
