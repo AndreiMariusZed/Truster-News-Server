b3467835d68e9e3006cd50cf17e875532ad09038
AndreiMariusZed <62403550+AndreiMariusZed@users.noreply.github.com>
2022-02-15 21:12:09 +0200

    aii

--- server/routes/article.js
+++ server/routes/article.js
@@ -16,18 +16,20 @@ router.post("/articles", upload.single("photo"), async (req, res) => {
     article.duration = req.body.duration;
     article.description = req.body.description;
 
-    // var spawn = require("child_process").spawn;
-    // var process = spawn("python", [
-    //   "./routes/predict.py",
-    //   req.body.title,
-    //   req.body.content,
-    // ]);
-    // process.stdout.on("data", async function (data) {
-    //   console.log(data.toString());
-    //   console.log("dupa save");
-    // });
-    await article.save();
-    res.json({ success: true, message: "Successfully saved article" });
+    var spawn = require("child_process").spawn;
+    var process = spawn("python", [
+      "D:/licenta/server/ai/predict.py",
+      req.body.title,
+      req.body.content,
+    ]);
+    // console.log(process);
+    process.stdout.on("data", async function (data) {
+      console.log(data.toString());
+      console.log("dupa save");
+      // await article.save();
+      // res.json({ success: true, message: "Successfully saved article" });
+    });
+    console.log("bbb");
   } catch (err) {
     res.status(500).json({
       success: false,
